f2d64d469900938a3afade088c19e0c54da452e3
syadII <daysbiii@hotmail.com>
2021-06-08 21:43:14 -0700

    Add files via upload

---
+++ HW5/script.js
@@ -0,0 +1,9 @@
+ var timeNow = moment().format('MMMM Do YYYY, h: mm: ss a');
+ var hourNow = moment().format('h');
+ var hourNow24 = parseInt(moment().format('HH'));
+ var hourNowInt = parseInt(hourNow);
+ var Timer;
+(
+ var hour1 = $('#time1').text();
+ var hour2 = $('#time2').text();
+ var hour3 = $('#time3').text();
